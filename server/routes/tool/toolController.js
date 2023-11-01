8d46c191f1ceac6e8d486e2ca9c40e86404eb7ef
yuntae0401 <116324939+yuntae0401@users.noreply.github.com>
2023-11-02 00:23:00 +0900

    기자재 업로드 api

--- server/routes/tool/toolController.js
+++ server/routes/tool/toolController.js
@@ -5,28 +5,31 @@ module.exports = {
 
     addTool: async (req, res) => {
         const body = req.body;
-       // console.log(req)
+       console.log(body)
         const img = req.file;
         
         toolService.addTool(body, img)
           .then(result => {
-            console.log(result)
+          console.log(result)
           
-            
             let obj = {};
-            if (result == "err") {
+            if (result == "EXIST") { 
               obj["suc"] = false;
-              obj["error"] = errorCode.E05.message;
+              obj["error"] = errorCode.E06.message;
               res.send(obj);
-            } else if (result == false) {
+            } else if (result == "Value Null") {
               obj["suc"] = false;
-              obj["error"] = errorCode.E00.message;;
+              obj["error"] = errorCode.E07.message;
               res.send(obj);
             } 
-            else if(result=="Img Miss"){
+            else if(result=="img undefined"){
               obj['suc'] = false;
-              obj['result'] = "Img Miss";
-             res.send(obj);
+              obj['result'] = errorCode.E08.message;
+              res.send(obj);
+            }
+            else if(result===false){
+              obj['suc'] = false;
+              obj['result'] = errorCode.E00.message;
             }
             else {
               obj['suc'] = true;
--- server/routes/tool/toolService.js
+++ server/routes/tool/toolService.js
@@ -6,144 +6,71 @@ const {
 moment.tz.setDefault("Asia/Seoul");
 
 module.exports = {
-    addTool: (body, img) => {
+      addTool: (body, img) => {
         return new Promise((resolve) => {
-          Tool.findOrCreate({
-            where: {
-              [Op.or]: [
-                { tool_id: body.tool_id },
-                //{ tool_code: body.tool_code }
-              ]
-            },
-            defaults: {
-              tool_id: body.tool_id,
-              tool_division: body.tool_division,
-              tool_code: body.tool_code,
-              tool_name: body.tool_name,
-              tool_purchase_division: body.tool_purchase_division,
-              tool_purchase_date: moment().format("YYYY-MM-DD"),
-              tool_update_at: moment().format("YYYY-MM-DD"),
-              tool_state: "대여가능",
-              department_id: body.department_id
-            }
-          })
+          if (img == undefined) {
+            console.log("이거동작하니?")
+            resolve("img undefined");
+          } else if (
+            !body.tool_id ||
+            !body.tool_division ||
+            !body.tool_code ||
+            !body.tool_name ||
+            !body.tool_purchase_division ||
+            !body.department_id
+          ) {
+            resolve("Value Null");
+          } else {
+            Tool.findOrCreate({
+              where: {
+                [Op.or]: [
+                  { tool_id: body.tool_id }
+                ]
+              },
+              defaults: {
+                tool_id: body.tool_id,
+                tool_division: body.tool_division,
+                tool_code: body.tool_code,
+                tool_name: body.tool_name,
+                tool_purchase_division: body.tool_purchase_division,
+                tool_purchase_date: moment().format("YYYY-MM-DD"),
+                tool_update_at: moment().format("YYYY-MM-DD"),
+                tool_state: "대여가능",
+                department_id: body.department_id
+              }
+            })
             .then(async (result) => {
-              let obj = {};
-              let results = [];
-              obj["result"] = result[0];
-              if (result[1]) {
-                if (img != null) {
+              if (!result[1]) {
+                // 이미 존재하는 레코드라면 "EXIST"를 반환
+                resolve("EXIST");
+              } else {
+                let obj = { result: result[0] };
+                let results = [];
+      
+                if (img != undefined) {
                   await Img.create({
                     img_url: img.location,
                     tool_id: body.tool_id,
                   })
-                    .then((imageResult) => {
-                      results.push(imageResult);
-                     // console.log(imageResult);
-                      obj["image"] = results;
-                    })
-                    .catch((err) => {
-                      resolve("err");
-                     // console.log(err);
-                    })
-                } 
-                else {
-                  obj["image"] = "Not exist";
+                  .then((imgResult) => {
+                    results.push(imgResult);
+                    obj.image = results;
+                    resolve(obj);
+                  })
+                  .catch((err) => {
+                    console.log(err);
+                    resolve(false); // 오류가 발생하면 false를 반환
+                  });
+                } else {
+                  resolve(obj);
                 }
-                resolve(obj);
-              } 
-
-              else if (img==null){
-                resolve("Img Miss")
               }
-              else if(!result[1]){
-
-                resolve("EXIST");
-              } 
-              else {
-                resolve(false);
-              }
-    
             })
-    
-          .catch((err) => {
-           // console.log(err);
-           console.log("이게동작하니")
-            resolve("err")
-          })
-        })
-      }, 
-
-      //이미지를 받고 이미지가 비어 있으면 이미지가 비어 있습니다를 출력
-      //값이 하나라도 비어 있으면 값이 비어있습니다. 를 출력
-      //id값이 중복된다면 아이디 값이 중복됩니다. 를 출력
-      //기자재를 추가하면 tool DB에 추가하고 IMG DB에 사진 업로드
-      /*  addTool: (body,img) =>{
-        return new Promise((resolve)=>{
-          if(img==undefined){
-            resolve("img undefined")
+            .catch((err) => {
+              console.log(err);
+              resolve(false); // 오류가 발생하면 false를 반환
+            });
           }
-          else if (!body.tool_id ||!body.tool_division ||!body.tool_code ||!body.tool_name ||!body.tool_purchase_division ||!body.department_id){
-            resolve("Value Null")
-          }
-
-          else{
-              Tool.findOrCreate({
-                where: {
-                  [Op.or]:[
-                    {tool_id: body.tool_id}
-                  ]
-                },
-                defaults: {
-                  tool_id: body.tool_id,
-                  tool_division: body.tool_division,
-                  tool_code: body.tool_code,
-                  tool_name: body.tool_name,
-                  tool_purchase_division: body.tool_purchase_division,
-                  tool_purchase_date: moment().format("YYYY-MM-DD"),
-                  tool_update_at: moment().format("YYYY-MM-DD"),
-                  tool_state: "대여가능",
-                  department_id: body.department_id
-                }
-              })
-              .then(async(result)=>{
-                let obj = {};
-                let results = [];
-                obj["result"]=result[0]
-                if(result[1]){
-                  if(img!=undefined){
-                    await Img.create({
-                      img_url: img.location,
-                      tool_id: body.tool_id,
-                    })
-                    .then((imgResult)=>{
-                      results.push(imgResult)
-                      obj["image"] = results;
-                      resolve(result)
-                    })
-                    .catch((err)=>{
-                      resolve("err")
-                    })
-                  }
-                  else if(results[1]){
-                   
-                    resolve(results)
-                  }
-                  
-                  else(
-                    
-                   resolve(false)
-                   
-                  )
-                  .catch((err)=>{
-                    resolve("err")
-                  }) 
-                  
-                }
-              })
-          }
-        })
+        });
       }
-*/
-     
 }
