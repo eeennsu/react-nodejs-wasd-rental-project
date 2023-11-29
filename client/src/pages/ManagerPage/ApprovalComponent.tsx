017a3b4a0cdc712983dad75f98c40bae60cf461d
eeennsu <diso592@naver.com>
2023-11-29 20:31:48 +0900

    조금 변경

--- client/src/pages/ManagerPage/ApprovalComponent.tsx
+++ client/src/pages/ManagerPage/ApprovalComponent.tsx
@@ -51,7 +51,7 @@ const ApprovalComponent : FC = () =>{
     { id: 2, 학번: '2021002', 이름: '김철수', 날짜: '2023-11-10' },
     { id: 3, 학번: '2021003', 이름: '이영희', 날짜: '2023-11-11' },
     { id: 4, 학번: '2021004', 이름: '박영수', 날짜: '2023-11-12' },
-    // { id: 5, 학번: '2021005', 이름: '박영수', 날짜: '2023-11-12' },
+    // { id: 5, 학번: '2021005', 이름: '박영수', 날짜: '2023-E11-12' },
     // { id: 6, 학번: '2021006', 이름: '박영수', 날짜: '2023-11-12' },
     // { id: 7, 학번: '2021007', 이름: '박영수', 날짜: '2023-11-12' },
     // { id: 8, 학번: '2021008', 이름: '박영수', 날짜: '2023-11-12' },
@@ -65,7 +65,7 @@ const ApprovalComponent : FC = () =>{
       
         <tbody>
           {applicants.map((applicant, index) => (
-            <React.Fragment key={applicant.id} index={index}> 
+            <React.Fragment key={applicant.id}> 
               <tr >
                 <td className="text-center">{applicant.학번}</td>
                 <td className="text-center">{applicant.이름}</td>
