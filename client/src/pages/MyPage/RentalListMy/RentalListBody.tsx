dbf40b7000692fbe1db2beca8d9c3abb4286d6fb
Lyeonu <yeonu363@gmail.com>
2023-12-24 11:14:03 +0900

    api 시도해보기...

--- client/src/pages/MyPage/RentalListMy/RentalListBody.tsx
+++ client/src/pages/MyPage/RentalListMy/RentalListBody.tsx
@@ -1,4 +1,6 @@
 import React, { useState, useEffect } from 'react';
+import Button from '../../../components/Button';
+import { useUserStore } from '../../../zustand';
 import type { FC } from 'react';
 
 interface RentalRecord {
@@ -8,13 +10,13 @@ interface RentalRecord {
 }
 
 const RentalListBody: FC = () => {
+  const { user } = useUserStore();
   const [rentalRecords, setRentalRecords] = useState<RentalRecord[]>([]);
 
   useEffect(() => {
-    const API_ENDPOINT = 'rental/myAllRentalList/:user_id/:page/:pageLimit';
+    const API_ENDPOINT = 'myAllRentalList_API_MY_PAGE';
 
     const fetchRentalRecords = async () => {
-      //... (이하 동일)
     };
 
     fetchRentalRecords(); 
@@ -30,8 +32,10 @@ const RentalListBody: FC = () => {
       ))}
 
             <div className='w-[700px] h-[50px] ml-[10px] bg-03 rounded-md flex items-center justify-center'>14번/오큘러스/대여중</div>
-            <div className='w-[100px] h-[50px] ml-[20px] bg-01 rounded-md flex items-center justify-center text-white'>연장</div>
-            <div className='w-[100px] h-[50px] ml-[20px] bg-01 rounded-md flex items-center justify-center text-white'>반납</div>
+
+            <Button bgColor='01'className='w-[100px] h-[50px] ml-[20px] rounded-md flex items-center justify-center text-white'>연장</Button>
+            <Button bgColor='01'className='w-[100px] h-[50px] ml-[20px] rounded-md flex items-center justify-center text-white'>반납</Button>
+
     </div>
   );
 };
--- client/src/pages/MyPage/Request/RequestMy.tsx
+++ client/src/pages/MyPage/Request/RequestMy.tsx
@@ -8,7 +8,7 @@ const RequestMy: FC = () => {
       <div className='w-[700px] h-52 absolute top-[200px] ml-[450px] bg-02 rounded-md flex items-center justify-center'>
         
         <div className=''>수리요청</div>
-        <div className='w-[60px] h-[30px] ml-[200px] bg-01 rounded-md flex items-center justify-center text-white'>연장</div>
+        <Button bgColor='01'className='w-[60px] h-[30px] ml-[200px] rounded-md flex items-center justify-center text-white'>반납</Button>
         <div className=' ml-[200px] '>2023.10.08</div>
 
         <div className="h-px bg-white w-full absolute top-1/2 left-0 transform -translate-y-1/2"></div>
